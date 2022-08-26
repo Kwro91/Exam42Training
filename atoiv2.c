3ae74292f8a9533cccc42c49e6a5c8806e0cf06d
Benjamin Salort <besalort@e3r3p9.clusters.42paris.fr>
2022-08-26 14:34:16 +0200

    true atoiv2

---
+++ atoiv2.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: besalort <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/18 16:40:38 by besalort          #+#    #+#             */
+/*   Updated: 2022/08/18 17:16:29 by besalort         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+
+int	ft_atoi(char *str)
+{
+	int	i;
+	int	signe;
+	int	nb;
+
+	i = 0;
+	signe = 1;
+	nb = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			signe = -signe;
+		i++;
+	}
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		nb = nb * 10 + (str[i] - 48);
+		i++;
+	}
+	return (nb * signe);
+}
+
+int	main(int argc, char **argv)
+{
+	(void) argc;
+	printf("%d", atoi(argv[1]));
+}
